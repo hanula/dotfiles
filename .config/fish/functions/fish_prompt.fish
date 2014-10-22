34d78a9f2d4df81c2c82a6d951747902933234aa
Sebastian Hanula <sebastian.hanula@gmail.com>
2014-10-22 17:02:42 +0200

    Nicer colors for git+fish_prompt.

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -3,11 +3,12 @@ function fish_prompt
     echo -n "$USER@"
     echo -n (hostname)
     echo -n " "
-    set_color $fish_color_cwd
+    set_color cyan
     echo -n (prompt_pwd)
     set_color 3e3e3e
     vcprompt -f " (%b%m%u)"
-    set_color normal
+#    set_color normal
+    set_color 9ae 
     echo -n '> '
 end
 
