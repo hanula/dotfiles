2c1ca49fcf4f6c882952cea9b5fbbefd84133a94
Sebastian Hanula <sebastian.hanula@gmail.com>
2016-12-06 23:43:42 +0100

    fish: Add `tmuxifier` bin PATH.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 
 /usr/bin/archey3
 
-set PATH $PATH $HOME/bin $HOME/.gem/ruby/2.2.0/bin $HOME/dev/go/bin
+set PATH $PATH $HOME/bin $HOME/.gem/ruby/2.2.0/bin $HOME/dev/go/bin $HOME/.tmuxifier/bin
 
 alias subl subl3
 alias yaourt-update-aur "yaourt -Sbu --aur"
