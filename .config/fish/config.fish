39e792a3a7ed23a863ba38f560b69657264ef01e
Sebastian Hanula <sebastian.hanula@gmail.com>
2016-12-06 23:44:10 +0100

    fish: Add `ANDROID_HOME` env var.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,7 @@
 /usr/bin/archey3
 
 set PATH $PATH $HOME/bin $HOME/.gem/ruby/2.2.0/bin $HOME/dev/go/bin $HOME/.tmuxifier/bin
+set ANDROID_HOME /opt/android-sdk/
 
 alias subl subl3
 alias yaourt-update-aur "yaourt -Sbu --aur"
