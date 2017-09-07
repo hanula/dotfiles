1505366d77890d5f6210a072b206a10194c90b3e
Sebastian Hanula <sebastian.hanula@gmail.com>
2017-09-07 20:44:05 +0200

    fish: Add firefox as default env `BROWSER`.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,7 @@
 set PATH $PATH $HOME/bin $HOME/.gem/ruby/2.2.0/bin $HOME/dev/go/bin $HOME/.tmuxifier/bin
 set ANDROID_HOME /opt/android-sdk/
 set -x EDITOR vim
+set -x BROWSER firefox
 
 alias subl subl3
 alias yaourt-update-aur "yaourt -Sbu --aur"
