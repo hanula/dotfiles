a9209c0215fba3151bb634cbbcab77847b0ab683
Sebastian Hanula <sebastian.hanula@gmail.com>
2017-09-07 20:44:28 +0200

    fish: Add `cargo` bin to the `PATH`.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 
 /usr/bin/archey3
 
-set PATH $PATH $HOME/bin $HOME/.gem/ruby/2.2.0/bin $HOME/dev/go/bin $HOME/.tmuxifier/bin
+set PATH $PATH $HOME/bin $HOME/.gem/ruby/2.2.0/bin $HOME/dev/go/bin $HOME/.tmuxifier/bin $HOME/.cargo/bin
 set ANDROID_HOME /opt/android-sdk/
 set -x EDITOR vim
 set -x BROWSER firefox
